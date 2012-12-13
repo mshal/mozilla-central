f5a63c641475888d84dbfd6e0469ae36d0b0524f
Nathan Froyd <froydnj@mozilla.com>
2012-12-13 11:26:33 -0500

    Bug 821324 - actually return a success value from replaceFileIfChanged; r=bz

--- dom/bindings/Codegen.py
+++ dom/bindings/Codegen.py
@@ -38,6 +38,7 @@ def replaceFileIfChanged(filename, newContents):
     f = open(filename, 'wb')
     f.write(newContents)
     f.close()
+    return True
 
 def toStringBool(arg):
     return str(not not arg).lower()
