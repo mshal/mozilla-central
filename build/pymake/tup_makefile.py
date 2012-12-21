1408c8ac9fd5a6b0fe2d7b19b967a3dc8cb88b48
Mike Shal <mshal@mozilla.com>
2012-12-21 16:46:30 -0500

    Add more default directories

--- build/pymake/tup_makefile.py
+++ build/pymake/tup_makefile.py
@@ -35,7 +35,15 @@ class TupMakefile(object):
             self.enabled_dirs[tier] = True
 
         # TODO: Where are these enabled? They aren't part of tier_platform_dirs
-        for dirname in ['browser', 'chrome', 'js/src', 'modules/zlib', 'db/sqlite3/src']:
+        for dirname in [
+                'browser',
+                'chrome',
+                'config',
+                'db/sqlite3/src',
+                'js/src',
+                'memory/mozjemalloc',
+                'modules/zlib',
+                ]:
             tier = os.path.join(moz_root, dirname)
             self.enabled_dirs[tier] = True
 
