1717af687cfaee504379f05b13ba5e887b4ba4ee
Mike Shal <mshal@mozilla.com>
2012-12-21 15:34:05 -0500

    Enable js/src by default.

--- build/pymake/tup_makefile.py
+++ build/pymake/tup_makefile.py
@@ -34,7 +34,7 @@ class TupMakefile(object):
             self.enabled_dirs[tier] = True
 
         # TODO: Where are these enabled? They aren't part of tier_platform_dirs
-        for dirname in ['browser', 'chrome']:
+        for dirname in ['browser', 'chrome', 'js/src']:
             tier = os.path.join(moz_root, dirname)
             self.enabled_dirs[tier] = True
 
