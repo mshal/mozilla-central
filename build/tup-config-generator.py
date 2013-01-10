a0b0bf138c357d742eef60b66fce11c5f39c81d7
Mike Shal <mshal@mozilla.com>
2013-01-09 19:37:54 -0500

    Install prcpucfg.h.
    For some reason this goes in both dist/include and dist/include/nspr. The make build uses a symlink in dist/include/nspr, but makes a copy in dist/include. We just use a symlink for both. This is based on the value of the MDCPUCFG_H variable, which comes from the nspr autoconf.mk file.

--- build/tup-config-generator.py
+++ build/tup-config-generator.py
@@ -2,12 +2,14 @@
 
 import sys
 import os
+import re
 
-if len(sys.argv) < 2:
-    sys.exit('usage: %s path/to/config.status path/to/tup.config' % sys.argv[0])
+if len(sys.argv) < 4:
+    sys.exit('usage: %s path/to/config.status path/to/nsprpub/autoconf.mk path/to/tup.config' % sys.argv[0])
 
 config_status_path = sys.argv[1]
-tup_config_path = sys.argv[2]
+nsprpub_path = sys.argv[2]
+tup_config_path = sys.argv[3]
 
 sys.path.append(os.path.dirname(config_status_path))
 from configstatus import defines, substs, non_global_defines
@@ -20,3 +22,14 @@ for name, value in defines:
 for name, value in substs:
     # TODO: Expand $(FOO) and ${FOO} here?
     print >> output, "CONFIG_%s=%s" % (name, value)
+
+for line in open(nsprpub_path, 'r'):
+    m = re.match("([A-Z_]*)[ \t]*= *(.*)", line)
+    if m:
+        if m.group(1) in ['MDCPUCFG_H',
+                          'PR_MD_CSRCS',
+                          'PR_MD_ASFILES',
+                          'PR_MD_ARCH_DIR',
+                          'CPU_ARCH',
+                          ]:
+            print >> output, "CONFIG_%s=%s" % (m.group(1), m.group(2))
