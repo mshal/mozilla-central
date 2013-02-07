c2b3284ec324633c48235f7254c5a1b20fe63da2
Mike Shal <mshal@mozilla.com>
2013-02-06 21:28:58 -0500

    Compile js/src/ctypes/libffi/
    This one is a special case. Here we try to parse libffi's Makefile that is generated from its own configure, since it is using automake.

---
+++ build/pymake/tup_libffi.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import sys
+import os
+import tup_makefile
+
+if len(sys.argv) < 3:
+    sys.exit('usage: %s MOZ_ROOT MOZ_OBJDIR' % sys.argv[0])
+
+moz_root = sys.argv[1]
+moz_objdir = sys.argv[2]
+tupmk = tup_makefile.TupMakefile(moz_root, moz_objdir, makefile_name='Makefile',
+                                 always_enabled=True)
+
+libffi_path = os.path.join(moz_root, moz_objdir, 'js', 'src', 'ctypes', 'libffi')
+tupmk.parse(libffi_path)
+
+srcs = tupmk.get_var('SOURCES')
+
+# This srcdir goes through js/src/ctypes/libffi, and we use it so we can grep
+# out full paths which mess with tup's dependency detection.
+srcdir = tupmk.get_var('abs_srcdir')[0]
+
+all_flags = []
+for flag in tupmk.get_var('COMPILE'):
+    # Trim down full paths so that they are local to libffi
+    if flag.startswith('-I' + srcdir):
+        all_flags.append('-I' + flag[len('-I') + len(srcdir) + 1:])
+    else:
+        all_flags.append(flag)
+
+all_flags.append('-I' + libffi_path)
+
+if srcs:
+    # Compile each src file
+    print ": foreach",
+    print ' '.join(srcs),
+    print "|> ^ CC %f^",
+    print ' '.join(all_flags),
+    print "-c %f -o %o",
+    print "|> %B.o"
+
+    # Link them into libffi.a
+    print ": *.o |> ^ AR %o^ ar crs %o %f |> libffi.a"
