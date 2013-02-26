31e72edfd8cce09ccf30eb7d8954c12ae01a18d8
Mike Shal <mshal@mozilla.com>
2013-02-26 17:02:53 -0500

    Remove manual mozilla-config.h include.
    The mozilla-config.h file is included automatically.

--- editor/libeditor/html/nsHTMLEditUtils.cpp
+++ editor/libeditor/html/nsHTMLEditUtils.cpp
@@ -3,7 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "./../../mozilla-config.h"     // for MOZ_MEDIA
 #include "mozilla/Assertions.h"         // for MOZ_ASSERT, etc
 #include "mozilla/Util.h"               // for ArrayLength
 #include "mozilla/dom/Element.h"        // for Element, nsINode
