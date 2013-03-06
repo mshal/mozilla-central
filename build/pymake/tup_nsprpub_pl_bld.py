314f499b329fa566d827d2c5be3730247bdfa67f
Mike Shal <mshal@mozilla.com>
2013-03-06 18:11:26 -0500

    Compile nsprpub/lib/libc/src/

--- build/pymake/tup_nsprpub_pl_bld.py
+++ build/pymake/tup_nsprpub_pl_bld.py
@@ -20,16 +20,18 @@ cpp = tup_cpp.TupCpp(tupmk, moz_objdir, target_srcs_flag=True, nsprpub=True,
 
 tupmk.parse('.')
 
+config_now = "%s/nsprpub/config/now" % (moz_root)
+
 suffix = tupmk.get_var_string('SUF')
 prod = "lib%s%s.%s" % (tupmk.get_var_string('LIBRARY_NAME'),
                        tupmk.get_var_string('LIBRARY_VERSION'),
                        tupmk.get_var_string('DLL_SUFFIX'))
 gen_pl_bld = """shdate=`date "+%%Y-%%m-%%d %%T"`; """
-gen_pl_bld += """shnow=`../../config/now`; """
+gen_pl_bld += """shnow=`%s`; """ % (config_now)
 gen_pl_bld += """(echo "#define _BUILD_STRING \\"$shdate\\""; """
 gen_pl_bld += """if test ! -z "$shnow"; then echo "#define _BUILD_TIME ${shnow}%s"; fi; """ % (suffix)
 gen_pl_bld += """echo "#define _PRODUCTION \\"%s\\"") > %%o""" % (prod)
 
-print ": ../../config/now |> %s |> _pl_bld.h" % (gen_pl_bld)
+print ": %s |> %s |> _pl_bld.h" % (config_now, gen_pl_bld)
 
 cpp.generate_cpp_rules()
