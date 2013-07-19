1330c1b0cd411a41b4f89bada3aed7c50f805e20
Mike Shal <mshal@mozilla.com>
2013-07-19 10:28:34 -0400

    TMP2: use Makefile.in too

---
+++ python/mozbuild/mozbuild/backend/tup/exports.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+def generate_exports(exports, namespace=""):
+    strings = exports.get_strings()
+    if namespace:
+        namespace += '/'
+    for export in strings:
+        print ": foreach %s |> !cp |> $(MOZ_ROOT)/dist/include/%s%%b | $(MOZ_ROOT)/<installed-headers>" % (export, namespace)
+
+    children = exports.get_children()
+    for subdir in sorted(children):
+        generate_exports(children[subdir], namespace=namespace + subdir)
+
+def generate_rules(sandbox):
+    generate_exports(sandbox['EXPORTS'])
