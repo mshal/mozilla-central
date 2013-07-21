e5630569f42f72e9ca96f8def41d0fd0d31798dd
Mike Shal <mshal@mozilla.com>
2013-07-21 10:59:39 -0400

    Move relativesrcdir to the sandbox

--- python/mozbuild/mozbuild/backend/tup/mozbuildmakesandbox.py
+++ python/mozbuild/mozbuild/backend/tup/mozbuildmakesandbox.py
@@ -3,6 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import os
 import sys
 from mozbuild.frontend.reader import MozbuildSandbox
 
@@ -15,6 +16,17 @@ class MozbuildMakeSandbox(MozbuildSandbox):
         self.makefile = None
         self.objs = []
         self.moz_root = moz_root
+
+        # Get the path relative to moz_root by finding the components of cwd
+        # using the length of moz_root. Eg, if our cwd is
+        # HOME/m-c/xpcom/base, then moz_root is "../..", so we count one '/'
+        # in moz_root, and add one to it so path_count is 2. We pull off the
+        # last 2 parts of cwd to get "xpcom/base", and prefixed with
+        # moz_root becomes "../../xpcom/base".
+        cwd = os.getcwd()
+        cwd_parts = cwd.split('/')
+        path_count = moz_root.count('/') + 1
+        self.relativesrcdir = os.path.join(*cwd_parts[-path_count:])
 
     def get_string(self, name):
         value = self[name]
