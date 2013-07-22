5adbd2013f2f69e979a27b8dd362cec54ba6b92a
Mike Shal <mshal@mozilla.com>
2013-07-22 16:54:14 -0400

    Remove custom copy rules for configure-generated headers.

--- python/mozbuild/mozbuild/backend/tup/exports.py
+++ python/mozbuild/mozbuild/backend/tup/exports.py
@@ -3,16 +3,21 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-def generate_exports(exports, namespace=""):
+import os
+
+def generate_exports(exports, outputdir, namespace=""):
     strings = exports.get_strings()
     if namespace:
         namespace += '/'
     for export in strings:
-        print ": foreach %s |> !cp |> $(DIST)/include/%s%%b | $(MOZ_ROOT)/<installed-headers>" % (export, namespace)
+        # Install the file from the srcdir if it exists, otherwise try to
+        # install it from the objdir. Some files (eg: headers generated from
+        # configure) are in the objdir.
+        print ": | $(MOZ_ROOT)/<generated-headers> |> ^ INSTALL %s^ if [ -f %s ]; then cp %s %%o; else cp %s/%s %%o; fi |> $(DIST)/include/%s%s | $(MOZ_ROOT)/<installed-headers>" % (export, export, export, outputdir, export, namespace, os.path.basename(export))
 
     children = exports.get_children()
     for subdir in sorted(children):
-        generate_exports(children[subdir], namespace=namespace + subdir)
+        generate_exports(children[subdir], outputdir, namespace=namespace + subdir)
 
 def generate_rules(sandbox):
-    generate_exports(sandbox['EXPORTS'])
+    generate_exports(sandbox['EXPORTS'], sandbox.outputdir)
