eadf44aeb49471c2f2c245440dec767752a99f60
Mike Shal <mshal@mozilla.com>
2013-07-30 15:04:03 -0400

    Built intl/

--- python/mozbuild/mozbuild/backend/tup.py
+++ python/mozbuild/mozbuild/backend/tup.py
@@ -115,6 +115,9 @@ if __name__ == '__main__':
     elif sandbox.relativesrcdir == 'gfx/thebes':
         from tup import gfx_thebes
         gfx_thebes.generate_rules(sandbox)
+    elif sandbox.relativesrcdir == 'intl/locale/src':
+        from tup import intl_locale_src
+        intl_locale_src.generate_rules(sandbox)
 
     if 'all_webidl_files' in sandbox:
         from tup import dombindings
@@ -128,7 +131,7 @@ if __name__ == '__main__':
     if 'EXPORTS_NAMESPACES' in sandbox:
         from tup import oldexports
         oldexports.generate_rules(sandbox)
-    if 'CPP_SOURCES' in sandbox and (sandbox.relativesrcdir.startswith('xpcom') or sandbox.relativesrcdir.startswith('a') or sandbox.relativesrcdir.startswith('dom') or sandbox.relativesrcdir.startswith('content') or sandbox.relativesrcdir.startswith('editor') or sandbox.relativesrcdir.startswith('embedding') or sandbox.relativesrcdir.startswith('extensions') or sandbox.relativesrcdir.startswith('gfx') or sandbox.relativesrcdir.startswith('hal') or sandbox.relativesrcdir.startswith('image')):
+    if 'CPP_SOURCES' in sandbox and (sandbox.relativesrcdir.startswith('xpcom') or sandbox.relativesrcdir.startswith('a') or sandbox.relativesrcdir.startswith('dom') or sandbox.relativesrcdir.startswith('content') or sandbox.relativesrcdir.startswith('editor') or sandbox.relativesrcdir.startswith('embedding') or sandbox.relativesrcdir.startswith('extensions') or sandbox.relativesrcdir.startswith('gfx') or sandbox.relativesrcdir.startswith('hal') or sandbox.relativesrcdir.startswith('image') or sandbox.relativesrcdir.startswith('intl')):
         from tup import cpp
         cpp.generate_rules(sandbox, extra_includes=options.tup_extra_includes)
         # TODO: libvpx
