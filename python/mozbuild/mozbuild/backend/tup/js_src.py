34290bb97adfc762614eb4d197daece35886d9b2
Mike Shal <mshal@mozilla.com>
2013-08-01 16:04:08 -0400

    Convert HOST_CPPSRCS

--- python/mozbuild/mozbuild/backend/tup/js_src.py
+++ python/mozbuild/mozbuild/backend/tup/js_src.py
@@ -4,10 +4,6 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 def generate_rules(sandbox):
-    from tup import cpp
-    tupcpp = cpp.TupCpp(sandbox, host_srcs_flag=True, js_src=True)
-    tupcpp.generate_cpp_rules()
-
     print ": host_jsoplengen |> ./%f %o |> jsautooplen.h | $(MOZ_ROOT)/<generated-headers>"
     print ": host_jskwgen |> ./%f %o |> jsautokw.h | $(MOZ_ROOT)/<generated-headers>"
     print ": |> ^ embedjs.py -> %%o^ $(PYTHON) builtin/embedjs.py %s -p '%s' -m js.msg -o %%o %s |> selfhosted.out.h | selfhosted.js $(MOZ_ROOT)/<generated-headers>" % (sandbox.get_string('SELFHOSTED_DEFINES'), sandbox.get_string('CPP'), sandbox.get_string('selfhosting_srcs'))
