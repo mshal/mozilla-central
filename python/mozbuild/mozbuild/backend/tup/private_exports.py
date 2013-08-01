fb4fba68eec51c465ec3bb698dde0a433db160bf
Mike Shal <mshal@mozilla.com>
2013-07-31 20:33:02 -0400

    More nss

---
+++ python/mozbuild/mozbuild/backend/tup/private_exports.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+
+def generate_rules(sandbox):
+    exports = sandbox['PRIVATE_EXPORTS']
+    # This is specific to security/nss
+    for export in exports:
+        print ": %s |> ^ INSTALL %%f^ cp %%f %%o |> $(DIST)/private/nss/%%b | $(MOZ_ROOT)/<installed-headers>" % (sandbox.vpath_resolve(export))
