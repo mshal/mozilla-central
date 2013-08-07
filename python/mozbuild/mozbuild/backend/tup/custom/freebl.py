7a369d4843686e375347ba5f0cdc4927c8c010b6
Mike Shal <mshal@mozilla.com>
2013-08-07 11:15:16 -0400

    It works for real this time!

--- python/mozbuild/mozbuild/backend/tup/custom/freebl.py
+++ python/mozbuild/mozbuild/backend/tup/custom/freebl.py
@@ -3,6 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import sys
 def generate_rules(sandbox):
     from tup import makefile_parser
     sandbox.set_var('FREEBL_CHILD_BUILD', '1')
@@ -23,4 +24,12 @@ def generate_rules(sandbox):
 
     from tup import csrcs
     csrcs.generate_rules(sandbox)
-    return
+
+    # Suffixes are annoying under nss. The .s files become .o, but .c files
+    # become 'o'. generate_nss_library() already adjusts all 'o' to '.o',
+    # so we have to make sure the asm files match.
+    simple_objs = [s.replace('.o', 'o') for s in sandbox['SIMPLE_OBJS']]
+    sandbox.set_var('SIMPLE_OBJS', simple_objs)
+
+    from tup import linker
+    linker.generate_nss_library(sandbox)
