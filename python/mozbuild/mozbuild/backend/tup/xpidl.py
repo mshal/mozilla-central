74e8fe607b26dedb0bf0b683cfd22b40b2313fe9
Mike Shal <mshal@mozilla.com>
2013-08-16 18:49:03 -0400

    Update xpidl since we no longer have XPIDL_FLAGS

--- python/mozbuild/mozbuild/backend/tup/xpidl.py
+++ python/mozbuild/mozbuild/backend/tup/xpidl.py
@@ -11,7 +11,6 @@ def generate_rules(sandbox):
             xpidl_module = sandbox['MODULE']
             if not xpidl_module:
                 return
-        flags = sandbox.get_string('XPIDL_FLAGS')
         for xpidl in xpidlsrcs:
             # Install the .idl file in dist/idl/
             print ": foreach %s |> !cp |> $(DIST)/idl/%%b | $(MOZ_ROOT)/<installed-idls>" % (xpidl)
@@ -19,13 +18,13 @@ def generate_rules(sandbox):
             # Generate the .h file locally. Put it in the {xpidl} bin for
             # each file so we don't have to duplicate path manipulations
             # here.
-            print ": foreach %s | $(MOZ_ROOT)/<installed-idls> |> ^ python header.py [%%f -> %%o]^ $(PYTHON_PATH) $(PLY_INCLUDE) $(MOZ_ROOT)/xpcom/idl-parser/header.py %s %%f -o %%o --cachedir=$(MOZ_ROOT)/xpcom/idl-parser |> %s/%%B.h | $(MOZ_ROOT)/<installed-headers> {%s}" % (xpidl, flags, sandbox.outputdir, xpidl)
+            print ": foreach %s | $(MOZ_ROOT)/<installed-idls> |> ^ python header.py [%%f -> %%o]^ $(PYTHON_PATH) $(PLY_INCLUDE) $(MOZ_ROOT)/xpcom/idl-parser/header.py -I$(DIST)/idl %%f -o %%o --cachedir=$(MOZ_ROOT)/xpcom/idl-parser |> %s/%%B.h | $(MOZ_ROOT)/<installed-headers> {%s}" % (xpidl, sandbox.outputdir, xpidl)
 
             # Install the .h file to dist/include
             print ": foreach {%s} |> !cp |> $(DIST)/include/%%b | $(MOZ_ROOT)/<installed-headers>" % (xpidl)
 
             # Generate the .xpt file
-            print ": foreach %s | $(MOZ_ROOT)/<installed-idls> |> ^ typelib.py %%o^ $(PYTHON_PATH) $(PLY_INCLUDE) -I$(MOZ_ROOT)/xpcom/typelib/xpt/tools $(MOZ_ROOT)/xpcom/idl-parser/typelib.py %s %%f --cachedir=$(MOZ_ROOT)/xpcom/idl-parser -o %%o |> %s/%%B.xpt {xpts}" % (xpidl, flags, sandbox.outputdir)
+            print ": foreach %s | $(MOZ_ROOT)/<installed-idls> |> ^ typelib.py %%o^ $(PYTHON_PATH) $(PLY_INCLUDE) -I$(MOZ_ROOT)/xpcom/typelib/xpt/tools $(MOZ_ROOT)/xpcom/idl-parser/typelib.py -I$(DIST)/idl %%f --cachedir=$(MOZ_ROOT)/xpcom/idl-parser -o %%o |> %s/%%B.xpt {xpts}" % (xpidl, sandbox.outputdir)
 
         # Link the module xpt
         print ": {xpts} |> ^ xpt.py link %%o^ $(PYTHON) $(MOZ_ROOT)/xpcom/typelib/xpt/tools/xpt.py link %%o %%f |> %s/%s.xpt {module_xpt}" % (sandbox.outputdir, xpidl_module)
